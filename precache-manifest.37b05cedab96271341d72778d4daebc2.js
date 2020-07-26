037f5f3f2d5c609e3d1134b018d0710430eccdba
Tai Bui <vantaibui92@gmail.com>
2020-07-26 23:01:05 +0700

    Updates

---
+++ precache-manifest.37b05cedab96271341d72778d4daebc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1501d0c5de3ea6c1c315a34d21d75d",
+    "url": "/burger-pages/index.html"
+  },
+  {
+    "revision": "930ad99faeaf6410010f",
+    "url": "/burger-pages/static/css/main.b7781c8b.chunk.css"
+  },
+  {
+    "revision": "cb0ed8a38e0e4171d668",
+    "url": "/burger-pages/static/js/2.7cd024be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-pages/static/js/2.7cd024be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "930ad99faeaf6410010f",
+    "url": "/burger-pages/static/js/main.b54408d8.chunk.js"
+  },
+  {
+    "revision": "ac20358d6019585335f1",
+    "url": "/burger-pages/static/js/runtime-main.f9d723de.js"
+  }
+]);
